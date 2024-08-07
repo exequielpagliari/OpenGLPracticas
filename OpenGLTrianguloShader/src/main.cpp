4f11b73ab4e8b4f2b17e169323dc7b136eb95c69
Exequiel Pagliari <exepa@hotmail.com>
2024-08-06 21:29:18 -0300

    Terminado capítulo Shaders

--- OpenGLTrianguloShader/src/main.cpp
+++ OpenGLTrianguloShader/src/main.cpp
@@ -7,6 +7,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <windows.h>
+#include "includes/Shader.h"
 void glfw_onError(int error, const char* description)
 {
 	// print message in Windows popup dialog box
@@ -15,19 +16,80 @@ void glfw_onError(int error, const char* description)
 #define  GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+/*
+//Ins and outs en shaders
 
 const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
+"layout(location = 0) in vec3 aPos; // position has attribute position 0\n"
+"out vec4 vertexColor; // specify a color output to the fragment shader\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+" gl_Position = vec4(aPos, 1.0); // we give a vec3 to vec4’s constructor\n"
+"vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // output variable to dark-red\n"
 "}\n\0";
+
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor; // input variable from vs (same name and type)\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+" FragColor = vertexColor;\n"
 "}\n\0";
+
+*/
+
+/*
+//Uniforms shaders
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor; // we set this variable in the OpenGL code.\n"
+"void main()\n"
+"{\n"
+" FragColor = ourColor;\n"
+"}\n\0";
+
+//Uniforms shaders
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"uniform vec4 ourColor; // we set this variable in the OpenGL code.\n"
+"void main()\n"
+"{\n"
+" FragColor = ourColor;\n"
+"}\n\0";*/
+/*
+//Uniforms shaders
+const char* vertexShaderSource = "#version 330 core\n"
+"layout(location = 0) in vec3 aPos; // position has attribute position 0\n"
+"layout(location = 1) in vec3 aColor; // color has attribute position 1\n"
+"out vec3 ourColor; // output a color to the fragment shader\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos, 1.0)\n;"
+"ourColor = aColor; // set ourColor to input color from the vertex data\n"
+"}\n\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+" FragColor = vec4(ourColor, 1.0);\n"
+"}\n\0";
+*/
 /*
 //Vertices del triángulo
 float vertices[] = {
@@ -35,22 +97,19 @@ float vertices[] = {
 0.5f, -0.5f, 0.0f,
 0.0f, 0.5f, 0.0f
 };
-
-unsigned int VBO, VAO;
 */
 
-//Múltiples Triángulos
+
 float vertices[] = {
-0.5f, 0.5f, 0.0f, // top right
-0.5f, -0.5f, 0.0f, // bottom right
--0.5f, -0.5f, 0.0f, // bottom left
--0.5f, 0.5f, 0.0f // top left
+	// positions // colors
+	0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+	0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
 };
-unsigned int indices[] = { // note that we start from 0!
-0, 1, 3, // first triangle
-1, 2, 3 // second triangle
-};
-unsigned int VBO, VAO, EBO;
+unsigned int VBO, VAO;
+
+
+
 
 void processInput(GLFWwindow* window)
 {
@@ -92,65 +151,9 @@ int main()
 		return -1;
 	}
 
-	//creación de shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	//carga y compilación de shader
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
-			infoLog << std::endl;
-	}
+	Shader ourShader("./src/shaders/Shader1.vs", "./src/shaders/Shader2.fs");
 
-
-
-	//creación y compilación de fragmentShader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
-			infoLog << std::endl;
-	}
-
-
-	//Creación del programa que linkea ambos shaders
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
-			infoLog << std::endl;
-	}
-
-
-
-
-	
-
-
-
-
-
-	/*
 	//Creación de polígono con VBO y VAO
 
 	glGenBuffers(1, &VBO);
@@ -162,8 +165,13 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //Triangulo Simple
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); //Doble Triángulo Shader
 	glEnableVertexAttribArray(0);
+
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); //Doble Triángulo Shader
+	glEnableVertexAttribArray(1);// Configuración para Doble Triángulo Shader
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -171,28 +179,8 @@ int main()
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
-	*/
 
 
-	////Creación de polígono con VBO, VAO y EBO
-	glGenBuffers(1, &VBO);
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &EBO);
-	// 1. bind Vertex Array Object
-	glBindVertexArray(VAO);
-	// 2. copy our vertices array in a vertex buffer for OpenGL to use
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3. copy our index array in a element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-		GL_STATIC_DRAW);
-	// 4. then set the vertex attributes pointers
-
-	// 4. then set the vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-		(void*)0);
-	glEnableVertexAttribArray(0);
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
@@ -204,10 +192,19 @@ int main()
 
 
 		render();
-		glUseProgram(shaderProgram);
+		/*
+		// update the uniform color
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram,
+			"ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		//
+		*/
+		ourShader.use();
+		ourShader.setFloat("someUniform", 1.0f);
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
